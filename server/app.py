18217edbba0c4a93251e4e0eb9871810db433da4
Yisel Cintora Vega <yiselcintoravega@gmail.com>
2024-06-24 15:53:28 -0400

    completed lab

--- server/app.py
+++ server/app.py
@@ -28,16 +28,38 @@ class Signup(Resource):
         return user.to_dict(), 201
 
 class CheckSession(Resource):
-    pass
+      def get(self):
+        if 'user_id' not in session:
+            return {}, 204
 
+        user_id = session['user_id']
+        user = User.query.get(user_id)
+
+        if user is None:
+            return {}, 204
+
+        return user.to_dict()
 class Login(Resource):
-    pass
+    def post(self):
+        user = User.query.filter(
+            User.username == request.get_json()['username']
+        ).first()
 
+        session['user_id'] = user.id
+        return user.to_dict()
+    
 class Logout(Resource):
-    pass
 
+    def delete(self): # just add this line!
+        session['user_id'] = None
+        return {'message': '204: No Content'}, 204
+    
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Logout, '/logout', endpoint='logout')
+api.add_resource(CheckSession,'/check_session', endpoint='check_session')
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
